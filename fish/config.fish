9361184ce33a70ebdc9dd470aefc97c4963ba762
Alain Armand <aarmand.inbox@gmail.com>
2020-12-12 23:40:54 +0100

    remove commented code

--- fish/config.fish
+++ fish/config.fish
@@ -1,11 +1,4 @@
-# set up paths early
-# function prepend_to_path -d "Prepend the given dir to PATH if it exists and is not already in it"
-#     if test -d $argv[1]
-#         if not contains $argv[1] $PATH
-#             set -gx PATH "$argv[1]" $PATH
-#         end
-#     end
-# end
+
 prepend_to_path "/usr/local/opt/sqlite/bin"
 prepend_to_path "/usr/local/opt/curl/bin"
 prepend_to_path "/usr/local/opt/ruby/bin"
