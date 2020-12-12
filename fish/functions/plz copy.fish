ff0f3b32b585527ae46484f69de35eb53462ac32
Alain Armand <aarmand.inbox@gmail.com>
2020-12-12 19:07:16 +0100

    ...

--- fish/functions/plz copy.fish
+++
@@ -1,9 +0,0 @@
-# function .runsudo --description 'Run current command as sudo.'
-# https://github.com/drhayes/drfish/blob/main/functions/.runsudo.fish
-function plz --description 'Run current command as sudo.'
-  commandline -C 0
-  commandline -i 'sudo '
-  commandline -f execute
-end
-
-bind \es plz
