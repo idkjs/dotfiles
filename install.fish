2d86fab4d3ffee30e4dae18f99bae79bc79d5194
Alain Armand <aarmand.inbox@gmail.com>
2020-12-13 20:13:55 +0100

    same for install.fish

--- install.fish
+++ install.fish
@@ -1,5 +1,5 @@
 #!/usr/bin/env fish
-set dotfiles $HOME/.dotfiles
+set dotfiles $HOME/dotfiles
 set symlinks $dotfiles/symlinks
 if test ! (which brew)
     /usr/bin/ruby -e "(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
@@ -30,15 +30,15 @@ brew tap Homebrew/bundle
 brew bundle
 
 mkdir -p $HOME/.config/fish
-ln -nFs $HOME/.dotfiles/fish/config.fish $HOME/.config/fish/config.fish
-ln -nFs $HOME/.dotfiles/fish/functions $HOME/.config/fish/functions
-ln -nFs $HOME/.dotfiles/fish/completions $HOME/.config/fish/completions
-ln -nFs $HOME/.dotfiles/fish/conf.d $HOME/.config/fish/conf.d
-ln -nFs $HOME/.dotfiles/symlinks/.curlrc $HOME/.curlrc
-ln -nFs $HOME/.dotfiles/symlinks/.editorconfig $HOME/.editorconfig
-ln -nFs $HOME/.dotfiles/symlinks/.inputrc $HOME/.inputrc
-ln -nFs $HOME/.dotfiles/symlinks/.vimrc $HOME/.vimrc
-ln -nFs $HOME/.dotfiles/symlinks/.dircolors $HOME/.dircolors
-ln -nFs $HOME/.dotfiles/symlinks/.hushlogin $HOME/.hushlogin
-ln -nFs $HOME/.dotfiles/symlinks/.rigreprc $HOME/.rigreprc
-ln -nFs $HOME/.dotfiles/symlinks/.wgetrc $HOME/.wgetrc
+ln -nFs $HOME/dotfiles/fish/config.fish $HOME/.config/fish/config.fish
+ln -nFs $HOME/dotfiles/fish/functions $HOME/.config/fish/functions
+ln -nFs $HOME/dotfiles/fish/completions $HOME/.config/fish/completions
+ln -nFs $HOME/dotfiles/fish/conf.d $HOME/.config/fish/conf.d
+ln -nFs $HOME/dotfiles/symlinks/.curlrc $HOME/.curlrc
+ln -nFs $HOME/dotfiles/symlinks/.editorconfig $HOME/.editorconfig
+ln -nFs $HOME/dotfiles/symlinks/.inputrc $HOME/.inputrc
+ln -nFs $HOME/dotfiles/symlinks/.vimrc $HOME/.vimrc
+ln -nFs $HOME/dotfiles/symlinks/.dircolors $HOME/.dircolors
+ln -nFs $HOME/dotfiles/symlinks/.hushlogin $HOME/.hushlogin
+ln -nFs $HOME/dotfiles/symlinks/.rigreprc $HOME/.rigreprc
+ln -nFs $HOME/dotfiles/symlinks/.wgetrc $HOME/.wgetrc
